3fda7720609572246c9efd58876313d80ae985a2
DingDean <dingkework@hotmail.com>
2018-08-01 21:12:24 +0800

    dev: use apiDoc instead of apiblueprint

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Mineself
+
+[![Build Status](https://travis-ci.com/DingDean/Mineself.svg?branch=master)](https://travis-ci.com/DingDean/Mineself)
+
 Mine Myself
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const compression = require('compression')
+const path = require('path')
+const fs = require('fs')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const app = express()
+const config = require('config')
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+/* MongoDB */
+const mongoose = require('mongoose')
+const MONGO_URL = config.get('mongo')
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URL, {
+  keepAlive: 120
+}).then(() => {
+  console.info('connected to database')
+}).catch(err => {
+  logger.error(err)
+  throw new Error(err)
 })
 
-const PORT = process.env.PORT || 3000
+/* Helmet */
+app.use(helmet())
+
+/* Setup Routes */
+app.use(compression())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const routes = config.get('routes')
+routes.forEach(({mount, component}) => {
+  app.use(
+    mount, 
+    require(`./src/routes/${component}`)
+  )
+})
+
+const PORT = config.get('port')
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
