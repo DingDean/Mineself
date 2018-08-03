024635faaf2c2ff4b39d89627177eed481e7d89b
DingDean <dingkework@hotmail.com>
2018-08-03 20:27:29 +0800

    database: finalize Tomato model

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,26 +1,46 @@
+const config = require('config')
 const mongoose = require('mongoose')
+const logger = require('./logger')
 
-mongoose.model('Session', mongoose.Schema({
+const Session = mongoose.model('Session', mongoose.Schema({
   start: {type: Date, default: Date.now},
-  inputTime: {type: Number, default: 0}, // time used in actual typing
-  totalTime: {type: Number, default: 0}, // total time
+  duratoin: {type: Number, default: 0}, // time used in ms
   filename: String,
   filetype: String,
   ticks: Number,
   project: String
 }))
 
-mongoose.model('Tomato', mongoose.Schema({
-  date: Number,
-  type: Number, // 0-finished, 1-abandoned
-  tStart: Number,
-  tEnd: Number
+const Tomato = mongoose.model('Tomato', mongoose.Schema({
+  isComplete: Boolean,
+  Start: Date,
+  minutes: Number,
+  thoughts: String
 }))
 
-mongoose.model('Search', mongoose.Schema({
+const Search = mongoose.model('Search', mongoose.Schema({
   originalUrl: { type: String, default: 'na' },
   source: { type: String, default: 'na' },
   topic: { type: String, default: 'na' },
   query: String,
   timestamp: Date
 }))
+
+function connect () {
+  const MONGO_URL = config.get('mongo')
+  mongoose.Promise = global.Promise
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    keepAlive: 120
+  }).then(() => {
+    logger.info('connected to database')
+  }).catch(err => {
+    logger.error(err)
+    throw new Error(err)
+  })
+}
+
+module.exports = {
+  Session, Tomato, Search,
+  connect
+}
